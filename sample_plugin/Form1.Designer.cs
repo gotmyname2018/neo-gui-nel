8bf2e818ce9aedf9f19dcf84815b032e9fba4904
lights <lightsever@hotmail.com>
2018-01-03 16:07:17 +0800

    nep5.1 研发中

--- sample_plugin/Form1.Designer.cs
+++ sample_plugin/Form1.Designer.cs
@@ -34,26 +34,42 @@
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.button3 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
-            this.button5 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
             this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
             this.button4 = new System.Windows.Forms.Button();
-            this.label8 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label9 = new System.Windows.Forms.Label();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.groupBox3.SuspendLayout();
             this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.groupBox6.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -71,7 +87,7 @@
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(466, 21);
             this.textBox1.TabIndex = 1;
-            this.textBox1.Text = "0x4055b11e8033486d724f5c18a0330cab16104abc";
+            this.textBox1.Text = "0x1258f1e3b146fb2cf2db7c5719dbc5eb9aa3f63a";
             // 
             // button1
             // 
@@ -116,23 +132,14 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "查询";
             // 
-            // button3
+            // label3
             // 
-            this.button3.Location = new System.Drawing.Point(23, 20);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(78, 23);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "查个人账户";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(85, 51);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(268, 21);
-            this.textBox2.TabIndex = 9;
-            this.textBox2.Text = "AdzQq1DmnHq86yyDUkU3jKdHwLUe2MLAVv";
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "label3";
             // 
             // label2
             // 
@@ -143,14 +150,23 @@
             this.label2.TabIndex = 10;
             this.label2.Text = "地址";
             // 
-            // label3
+            // textBox2
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 107);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 12);
-            this.label3.TabIndex = 11;
-            this.label3.Text = "label3";
+            this.textBox2.Location = new System.Drawing.Point(85, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(268, 21);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.Text = "AdzQq1DmnHq86yyDUkU3jKdHwLUe2MLAVv";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(23, 20);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(78, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "查个人账户";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // groupBox3
             // 
@@ -164,15 +180,14 @@
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "概览";
             // 
-            // button5
+            // label5
             // 
-            this.button5.Location = new System.Drawing.Point(21, 49);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(233, 23);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "发布（一次性，仅管理员账户有效）";
-            this.button5.UseVisualStyleBackColor = true;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(77, 17);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 12);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "管理员地址";
             // 
             // label4
             // 
@@ -183,14 +198,15 @@
             this.label4.TabIndex = 5;
             this.label4.Text = "管理员地址";
             // 
-            // label5
+            // button5
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(77, 17);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(65, 12);
-            this.label5.TabIndex = 6;
-            this.label5.Text = "管理员地址";
+            this.button5.Location = new System.Drawing.Point(21, 49);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(233, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "发布（一次性，仅管理员账户有效）";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // groupBox4
             // 
@@ -207,6 +223,40 @@
             this.groupBox4.TabIndex = 9;
             this.groupBox4.TabStop = false;
             this.groupBox4.Text = "转账";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(6, 67);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(53, 12);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "发送数量";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(79, 64);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 21);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.Text = "1";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(79, 36);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(268, 21);
+            this.textBox3.TabIndex = 12;
+            this.textBox3.Text = "AdzQq1DmnHq86yyDUkU3jKdHwLUe2MLAVv";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 39);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "接收地址";
             // 
             // label6
             // 
@@ -236,45 +286,153 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // label8
+            // groupBox5
             // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(6, 39);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(53, 12);
-            this.label8.TabIndex = 12;
-            this.label8.Text = "接收地址";
+            this.groupBox5.Controls.Add(this.label13);
+            this.groupBox5.Controls.Add(this.label12);
+            this.groupBox5.Controls.Add(this.label10);
+            this.groupBox5.Controls.Add(this.label11);
+            this.groupBox5.Controls.Add(this.textBox5);
+            this.groupBox5.Controls.Add(this.button2);
+            this.groupBox5.Location = new System.Drawing.Point(11, 20);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(359, 150);
+            this.groupBox5.TabIndex = 10;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "查询";
             // 
-            // textBox3
+            // label10
             // 
-            this.textBox3.Location = new System.Drawing.Point(79, 36);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(268, 21);
-            this.textBox3.TabIndex = 12;
-            this.textBox3.Text = "AdzQq1DmnHq86yyDUkU3jKdHwLUe2MLAVv";
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(21, 77);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(47, 12);
+            this.label10.TabIndex = 11;
+            this.label10.Text = "label10";
             // 
-            // textBox4
+            // label11
             // 
-            this.textBox4.Location = new System.Drawing.Point(79, 64);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 21);
-            this.textBox4.TabIndex = 13;
-            this.textBox4.Text = "1";
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(21, 54);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(29, 12);
+            this.label11.TabIndex = 10;
+            this.label11.Text = "地址";
             // 
-            // label9
+            // textBox5
             // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(6, 67);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(53, 12);
-            this.label9.TabIndex = 14;
-            this.label9.Text = "发送数量";
+            this.textBox5.Location = new System.Drawing.Point(85, 51);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(268, 21);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.Text = "AdzQq1DmnHq86yyDUkU3jKdHwLUe2MLAVv";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "查个人账户细节";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(21, 104);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(47, 12);
+            this.label12.TabIndex = 12;
+            this.label12.Text = "label12";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(21, 125);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(47, 12);
+            this.label13.TabIndex = 13;
+            this.label13.Text = "label13";
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.button8);
+            this.groupBox6.Controls.Add(this.button7);
+            this.groupBox6.Controls.Add(this.label14);
+            this.groupBox6.Controls.Add(this.textBox6);
+            this.groupBox6.Controls.Add(this.listBox2);
+            this.groupBox6.Controls.Add(this.button6);
+            this.groupBox6.Controls.Add(this.groupBox5);
+            this.groupBox6.Location = new System.Drawing.Point(12, 349);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Size = new System.Drawing.Size(700, 219);
+            this.groupBox6.TabIndex = 11;
+            this.groupBox6.TabStop = false;
+            this.groupBox6.Text = "NEP5.1 扩展";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(390, 20);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(105, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "查询基本情况";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(390, 49);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(304, 88);
+            this.listBox2.TabIndex = 12;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(9, 182);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(53, 12);
+            this.label14.TabIndex = 16;
+            this.label14.Text = "发送数量";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(82, 179);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 21);
+            this.textBox6.TabIndex = 15;
+            this.textBox6.Text = "1";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(222, 176);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(142, 23);
+            this.button7.TabIndex = 15;
+            this.button7.Text = "发送进池子里";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(390, 145);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 17;
+            this.button8.Text = "创建奖励";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(746, 466);
+            this.ClientSize = new System.Drawing.Size(742, 580);
+            this.Controls.Add(this.groupBox6);
             this.Controls.Add(this.groupBox4);
             this.Controls.Add(this.groupBox3);
             this.Controls.Add(this.groupBox2);
@@ -291,6 +449,10 @@
             this.groupBox3.PerformLayout();
             this.groupBox4.ResumeLayout(false);
             this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.groupBox6.ResumeLayout(false);
+            this.groupBox6.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -320,5 +482,19 @@
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.GroupBox groupBox6;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button8;
     }
 }
--- sample_plugin/Form1.cs
+++ sample_plugin/Form1.cs
@@ -94,6 +94,7 @@ namespace plugin_sample
                 if (bDebug)
                 {
                     engine = ApplicationEngine.RunWithDebug(tx.Script, tx);
+                    engine.FullLog.Save("d:\\0x00.llvmhex.txt");
                 }
                 else
                 {
@@ -230,7 +231,7 @@ namespace plugin_sample
                 };
                 var script = MakeAppCallScript(scripthash, p1, p2);
                 var ts = TestCallScript(script);
-                var n = (double)ts.GetBigInteger() / (double)amount;
+                var n = (decimal)ts.GetBigInteger() / (decimal)amount;
                 label3.Text = "balance:" + n;
             }
         }
@@ -266,7 +267,7 @@ namespace plugin_sample
             var fromscripthash = GetScriptHashFromAddress(label5.Text);
 
             var toscripthash = GetScriptHashFromAddress(textBox3.Text);
-            var num = (uint)(double.Parse(textBox4.Text) * (double)amount);
+            var num = new System.Numerics.BigInteger(decimal.Parse(textBox4.Text) * (decimal)amount);
             var p1 = new Neo.SmartContract.ContractParameter(Neo.SmartContract.ContractParameterType.String)
             {
                 Value = "transfer"
@@ -291,6 +292,159 @@ namespace plugin_sample
             };
             var script = MakeAppCallScript(scripthash, p1, p2);
             CallScript(script);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var scripthash = textBox1.Text;
+            var addrscripthash = GetScriptHashFromAddress(textBox5.Text);
+            {
+                var p1 = new Neo.SmartContract.ContractParameter(Neo.SmartContract.ContractParameterType.String)
+                {
+                    Value = "balanceOfDetail"
+                };
+                var p2 = new ContractParameter(Neo.SmartContract.ContractParameterType.Array)
+                {
+                    Value = new ContractParameter[]
+                    {
+                        new ContractParameter(Neo.SmartContract.ContractParameterType.ByteArray)
+                        {
+                            Value =addrscripthash
+                        }
+                    }
+                };
+                var script = MakeAppCallScript(scripthash, p1, p2);
+                var ts = TestCallScript(script);
+                var tss = ts.GetArray();
+                var cash = (decimal)tss[0].GetBigInteger() / (decimal)amount;
+                var saving = tss[1].GetBigInteger();
+                var savingblock = tss[2].GetBigInteger();
+                var balance = (decimal)tss[3].GetBigInteger() / (decimal)amount;
+                label10.Text = "balance:" + balance;
+                label12.Text = "cash(不可领奖部分):" + cash;
+                label13.Text = "saving(可领奖部分):" + saving + " block:[" + savingblock + "]";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            var script_hash = UInt160.Parse(textBox1.Text);
+            var key = System.Text.Encoding.ASCII.GetBytes("!pool");
+            var bchain = Neo.Core.Blockchain.Default;
+
+            var skey = new Neo.Core.StorageKey();
+            skey.Key = key;
+            skey.ScriptHash = script_hash;
+
+            listBox2.Items.Clear();
+
+            var item = bchain.GetStorageItem(skey);
+            if (item == null)
+            {
+                listBox2.Items.Add("pool empty");
+            }
+            else
+            {
+                listBox2.Items.Add("pool balance=" + (decimal)new System.Numerics.BigInteger(item.Value) / (decimal)amount);
+            }
+
+            var scripthash = textBox1.Text;
+            var addrscripthash = GetScriptHashFromAddress(textBox5.Text);
+            {
+                var p1 = new Neo.SmartContract.ContractParameter(Neo.SmartContract.ContractParameterType.String)
+                {
+                    Value = "checkBonus"
+                };
+                var p2 = new ContractParameter(Neo.SmartContract.ContractParameterType.Array)
+                {
+                };
+                var script = MakeAppCallScript(scripthash, p1, p2);
+                var ts = TestCallScript(script);
+                if (ts == null)
+                {
+                    listBox2.Items.Add("no Bonus.");
+                }
+                else
+                {
+                    try
+                    {
+                        var tss = ts.GetArray();
+                        foreach (var titme in tss)
+                        {
+                            if (titme.IsArray)
+                            {
+                                var arr = titme.GetArray();
+
+                                listBox2.Items.Add("领奖block=" + arr[0].GetBigInteger());
+                                listBox2.Items.Add("奖池总数=" + (decimal)arr[1].GetBigInteger() / (decimal)amount);
+                                listBox2.Items.Add("领奖比例=" + (decimal)arr[2].GetBigInteger() / (decimal)amount);
+                            }
+
+                        }
+                    }
+                    catch
+                    {
+                        listBox2.Items.Add("no bonus");
+                    }
+                }
+
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            var scripthash = textBox1.Text;
+
+            var fromscripthash = GetScriptHashFromAddress(label5.Text);
+            var num = new System.Numerics.BigInteger(decimal.Parse(textBox6.Text) * (decimal)amount);
+            var p1 = new Neo.SmartContract.ContractParameter(Neo.SmartContract.ContractParameterType.String)
+            {
+                Value = "use"
+            };
+            var p2 = new ContractParameter(Neo.SmartContract.ContractParameterType.Array)
+            {
+                Value = new ContractParameter[]
+                {
+                        new ContractParameter(Neo.SmartContract.ContractParameterType.ByteArray)
+                        {
+                            Value =fromscripthash
+                        },
+                         new ContractParameter(Neo.SmartContract.ContractParameterType.Integer)
+                        {
+                            Value =num
+                        }
+                }
+            };
+            var script = MakeAppCallScript(scripthash, p1, p2);
+            CallScript(script);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            var scripthash = textBox1.Text;
+            var addrscripthash = GetScriptHashFromAddress(textBox5.Text);
+            {
+                var p1 = new Neo.SmartContract.ContractParameter(Neo.SmartContract.ContractParameterType.String)
+                {
+                    Value = "checkBonusAndNew"
+                };
+                var p2 = new ContractParameter(Neo.SmartContract.ContractParameterType.Array)
+                {
+                };
+                var script = MakeAppCallScript(scripthash, p1, p2);
+                var ts = CallScript(script, true);
+                var sitem = ts.EvaluationStack.Pop();
+                if (sitem == null)
+                {
+                    listBox2.Items.Add("no Bonus.");
+                }
+                else
+                {
+                    var tss = sitem.GetBigInteger();
+                    listBox2.Items.Add("to bonus:" + tss);
+                }
+
+            }
         }
     }
 }
